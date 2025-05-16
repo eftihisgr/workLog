f88b59ed90ac9e2c5e86a6940a0e3df60f7dd3f5
Eftichios Papasifakis <csd5737@csd.uoc.gr>
2025-05-16 13:26:41 +0300

    fixed error in sw

--- sw.js
+++ sw.js
@@ -1,18 +1,25 @@
 const CACHE_NAME = 'worklog-v1';
 const ASSETS = [
-  '/WorkLog/',  // Match your repo name
+  '/WorkLog/',  // Match your GitHub repo name
   '/WorkLog/index.html',
   '/WorkLog/styles.css',
   '/WorkLog/script.js',
-  '/WorkLog/img/logo.png'
+  '/WorkLog/manifest.json',  // Don't forget this!
+  '/WorkLog/img/logo.png',
+  '/WorkLog/img/icon-192.png',
+  '/WorkLog/img/icon-512.png'
 ];
 
+// Install event: Cache all essential files
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))  // Fixed: Added comma
+  );
 });
 
+// Fetch event: Serve cached files when offline
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))  // Fixed: Added comma
+  );
 });
